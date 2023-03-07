7d0d0f4f494e713d826e38a9aaa7360a7f6e933e
MartinAlcaraz <96241107+MartinAlcaraz@users.noreply.github.com>
2023-03-07 10:05:46 -0300

    arreglado problema del localStorage al recuperar la variable 'user' por primera vez

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -7,6 +7,12 @@ export const UserContext = createContext();  // nombre del contexto  // UserCont
 export function UserContextProvider(props) {    // componente que contiene el contexto
 
     let u = JSON.parse(localStorage.getItem('user'));
+    // si user == null es porque todavia no se guardo la variable en el local storage
+    if(u == null){
+        u = {};
+        u.logged = false;
+        localStorage.setItem('user', JSON.stringify({ logged : false }));
+    }
     
     const [user, setUser] = useState(u);         // se crea la variable de estado global como user = false
 
@@ -15,7 +21,7 @@ export function UserContextProvider(props) {    // componente que contiene el co
     const checkIsLogged = async () => {
         let u = JSON.parse(localStorage.getItem('user'));
         setUser (u);
-        if ( u.logged == true ) {
+        if ( u != null && u.logged == true ) {
             const response = await LoginServices.isLogged();
             if (response.status == 200) {
                 console.log('Is logged');
